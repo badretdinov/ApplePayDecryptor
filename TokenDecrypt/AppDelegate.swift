8afa203fc616d1137cc1e46ccef5aa8148301023
Oleg Badretdinov <badretdinov@me.com>
2018-11-08 13:36:57 +0400

    Initial Commit

---
+++ TokenDecrypt/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TokenDecrypt
+//
+//  Created by Oleg Badretdinov on 08/11/2018.
+//  Copyright © 2018 Oleg Badretdinov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
